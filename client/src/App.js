fa63d28ed025e0a65e71cd01de7b255fca511476
saurabhsmishraa <saurabhpbh1999@gmail.com>
2024-02-28 01:36:05 +0530

    admin

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,28 @@ import Policy from './pages/Policy';
 import Pagenotfound from './pages/Pagenotfound';
 import Register from './pages/Auth/Register';
 import Login from "./pages/Auth/Login";
+import Dashboard from "./pages/user/Dashboard"
+import PrivateRoute from './components/Routes/Private';
+import ForgotPasssword from './pages/Auth/ForgotPassword';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import AdminRoute from './components/Routes/AdminRoute';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/dashboard' element={<PrivateRoute />}>
+          <Route path='user' element={<Dashboard />} />
+
+        </Route>
+        <Route path='/dashboard' element={<AdminRoute />}>
+          <Route path='admin' element={<AdminDashboard />} />
+
+        </Route>
         <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPasssword />} />
+
         <Route path='/login' element={<Login />} />
 
         <Route path='/about' element={<About />} />
