798e644cb21837577308be1144786956bfef2245
saurabh mishra <111421281+saurabhsmishraa@users.noreply.github.com>
2024-03-22 00:39:26 +0530

    Update server.js
    solve

--- server.js
+++ server.js
@@ -6,15 +6,12 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
-import {fileURLToPath} from "url";
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+
 
 //rest object
 const app = express();
@@ -23,7 +20,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './client/build')));
 
 
 //routes
