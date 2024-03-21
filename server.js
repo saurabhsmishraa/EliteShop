4781143ec8a1f7fcf1a35b0509de4fcce0e6a5bd
saurabh mishra <111421281+saurabhsmishraa@users.noreply.github.com>
2024-03-22 00:40:53 +0530

    Update server.js
    submit

--- server.js
+++ server.js
@@ -30,9 +30,7 @@ app.use("/api/v1/product", productRoutes);
 //rest api
 
 
-app.use('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-});
+
 
 
 app.get("/", (req, res) => {
