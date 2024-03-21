82e4147ae2393bce5bf4ce08bde6700e2a55dfc8
saurabhsmishraa <saurabhpbh1999@gmail.com>
2024-03-22 00:50:16 +0530

    update

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
 
 //configure env
 dotenv.config();
@@ -21,7 +20,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname, './client/build')));
 
 
 //routes
@@ -32,9 +30,6 @@ app.use("/api/v1/product", productRoutes);
 //rest api
 
 
-app.use('*', function (req, res) {
-    res.sendFile(path.join(__dirname, './client/build/index.html'))
-});
 
 
 app.get("/", (req, res) => {
